ac0768d4d37d3520b5387e159d327f75a0774de2
okwunna Anyaoku <okwunnaanyaoku@gmail.com>
2023-06-09 07:44:06 +0100

    data validation completed

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,9 +1,3 @@
-# This code is defining a data class called `DataIngestionConfig` using the `dataclass` decorator from
-# the `dataclasses` module. The class has four attributes: `root_dir`, `source_URL`,
-# `local_data_file`, and `unzip_dir`, all of which have specific types. `Path` is a class from the
-# `pathlib` module that represents a file system path. The `frozen=True` argument to the `dataclass`
-# decorator makes the class immutable, meaning that its attributes cannot be changed after it is
-# created.
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -13,3 +7,9 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
